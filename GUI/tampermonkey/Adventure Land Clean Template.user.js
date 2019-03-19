8f4a4dd15a159e5264d0a24029d753f5cc7422d4
Lucas Carnevalli <verdi@hotmail.com.br>
2019-03-19 14:12:17 -0300

    Update Adventure Land Clean Template.user.js

--- GUI/tampermonkey/Adventure Land Clean Template.user.js
+++ GUI/tampermonkey/Adventure Land Clean Template.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Adventure Land Clean Template
 // @version      0.1
-// @author       https://github.com/LVCarnevalli/adventureland
+// @author       lcarnevalli
 // @match        https://adventure.land/*
 // @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @require https://code.jquery.com/jquery-3.3.1.min.js
 // ==/UserScript==
 const $ = window.jQuery;
@@ -101,91 +103,164 @@ _action(
 GM_addStyle(`
 #topleftcorner,
 #toprightcorner,
-#bottomleftcorner2,
-#bottomleftcorner,
-#theskills,
 #topui,
 .menu {
-	padding: 5px !important;
+  padding: 5px !important;
 }
 
 #theskills {
-	bottom: 15px !important;
+  bottom: 28px !important;
+  padding-right: 5px !important;
 }
 
 #bottomleftcorner {
-	bottom: 15px !important;
+  bottom: 28px !important;
+  padding-left: 5px !important;
 }
 
 #bottomrightcorner {
-	bottom: 15px !important;
-	opacity: 0.7 !important;
-	padding-right: 5px !important;
+  bottom: 28px !important;
+  padding-right: 5px !important;
 }
 
 #bottomleftcorner2 {
-	bottom: 15px !important;
-	opacity: 0.7 !important;
+  bottom: 67px !important;
+  padding-left: 5px !important;
 }
 
 #chatinput {
-	bottom: 25px !important;
-	left: 10px !important;
-	opacity: 0.7 !important;
-}
-
-#toprightcorner .gamebutton,
-#topleftcornerui .gamebutton {
-	line-height: 10px !important;
-	font-size: 20px !important;
+  bottom: 28px !important;
+  left: 5px !important;
+  border: 3px groove #949494 !important;
 }
 
 .vtopx,
 .bshadow2 {
-	border: 0 !important;
-	padding: 1px !important;
+  padding: 0px !important;
+  border-left: 2px groove #949494 !important;
+  border-top: 2px groove #949494 !important;
+  border-bottom: 0 !important;
+}
+
+.vtopx {
+  border-top: 2px groove #949494 !important;
+  border-bottom: 0 !important;
+}
+
+#bottommid .enableclicks .bshadow2 {
+  border-bottom: 2px groove #949494 !important;
+}
+
+#bottommid .enableclicks > :last-child, #mpbar {
+  border-right: 2px groove #949494 !important;
 }
 
 .tint {
-	height: 20px !important;
-	line-height: 20px !important;
-	font-size: 20px !important;
+  height: 20px !important;
+  line-height: 20px !important;
+  font-size: 20px !important;
 }
 
 #rightcornerui>div {
-	border: 0px !important;
+  border: 3px groove #949494 !important;
+  background: url(https://i.stack.imgur.com/pMAiU.jpg) !important;
 }
 
 .salesui {
-	left: 5px !important;
-	bottom: 291px !important;
+  left: 5px !important;
+  bottom: 304px !important;
 }
 
 #features {
-	left: 5px !important;
-	bottom: 15px !important;
+  left: 5px !important;
+  bottom: 28px !important;
 }
 
 .xpsui {
-	width: auto!important;
-	margin-bottom: 0!important;
-	border: 0!important;
-	padding: 2px 0 0 0 !important;
+  width: auto!important;
+  margin-bottom: 0!important;
+  border: 0!important;
+  padding: 0 !important;
+  border-top: 2px groove #949494 !important;
 }
 
 #xpui {
-	width: 100%!important;
-	height: 7px !important;
-	line-height: 7px !important;
-	font-size: 0 !important;
+  width: 100%!important;
+  height: 20px !important;
+  line-height: 15px !important;
+  font-size: 20px !important;
+  color: #ffffff1f !important;
 }
 
 #bottommid {
-	width: 100%!important;
-	right: 0!important
+  width: 100%!important;
+  right: 0!important;
 }
 
 #xpslider {
-	height: 7px !important;
+  height: 20px !important;
+}
+
+#topui .largeborder, #features, .salesui, #gamelog, .menu .gamebutton,
+#pagewrapped, .menu > :last-child, .newcharacter .gamebutton, .selectioninput,
+.imodal, .imodal .gamebutton, #toprightcorner .gamebutton, #topleftcornerui .renderedinfo, #topleftcornerui .gamebutton, .modal .blockbutton, #bottomleftcorner > :first-child,
+#theskills > :last-child, #bottomleftcorner2 > :first-child, #bottomleftcorner2 > :last-child, #skillbar > :first-child, #topleftcornerui .slimbutton, #codeui, .engagebutton, .dengagebutton {
+  background: url(https://i.stack.imgur.com/pMAiU.jpg) !important;
+  border: 3px groove #949494 !important;
+}
+
+.CodeMirror-gutters, .CodeMirror-scroll {
+  background: url(https://i.stack.imgur.com/pMAiU.jpg) !important;
+}
+
+.CodeMirror-gutters .lspacer {
+  background: none !important;
+}
+
+.renderedinfo > :last-child > div, #bottomleftcorner > :first-child > :nth-child(3), #merchant-item > :first-child, #recipe-item > :first-child {
+  border: 0 !important;
+}
+
+.textbutton.chartype, .textbutton.gendertype, .friendslist, .renderedinfo > :last-child > div > div, #topleftcornerui > :last-child, #maincode, #topleftcornerui > :first-child, .inventory-item > :first-child, #skills-item > :first-child, #topleftcornerdialog > :first-child {
+  border: 3px groove #949494 !important;
+}
+
+#bottomleftcorner [ondrop="on_drop(event)"], #bottomrightcorner [ondrop="on_drop(event)"], #theskills [ondrop="on_drop(event)"], #topleftcornerui [style^="position: relative; display:inline-block; margin: 2px;"] {
+  border: 5px groove #949494 !important;
+}
+
+.noscroll::-webkit-scrollbar, .friendslist::-webkit-scrollbar {
+  background: url(https://i.stack.imgur.com/pMAiU.jpg) !important;
+}
+
+::-webkit-scrollbar-thumb {
+  background: rgba(200, 200, 200, 0.35) !important;
+}
+
+#theskills > :last-child > :first-child {
+  padding: 4px !important;
+  margin-left: 0 !important;
+}
+
+.friendslist {
+  overflow: overlay !important;
+}
+
+#chatwparty:not(.ui-draggable), div[id^='chatwpm']:not(.ui-draggable) {
+  bottom: 78px !important;
+  left: 5px !important;
+}
+
+#chatwparty, div[id^='chatwpm'] {
+  background: url(https://i.stack.imgur.com/pMAiU.jpg) !important;
+  border: 3px groove #949494 !important;
+}
+
+#chatwparty input, div[id^='chatwpm'] input {
+  border-top: 3px groove #949494 !important;
+}
+
+#chatwparty > :first-child, div[id^='chatwpm'] > :first-child {
+  border-bottom: 3px groove #949494 !important;
 }
 `);
